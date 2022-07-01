b00357060c4c78eabba408d5edc887f30e866d75
Connor Sahleen <connor.sahleen@gmail.com>
2022-07-01 06:29:17 -0500

    decorators.py
    Added timer decorator

--- decorators.py
+++ decorators.py
@@ -25,6 +25,19 @@ def count_calls(func):
         return func(*args, **kwargs)
     wrapper_count_calls.num_calls = 0
     return wrapper_count_calls
+    
+  
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
 
 
 def debug(func):
